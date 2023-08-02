b0a8de3339158b5b127fa256b9ce447b1240233a
Тимур Халилов <kotanonum@mail.ru>
2023-08-02 14:15:23 +0300

    основа плагина

--- src/main/java/org/lantern/Lantern.java
+++ src/main/java/org/lantern/Lantern.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.lantern.Sphere.Sphere;
@@ -25,5 +26,17 @@ public class Lantern extends JavaPlugin implements Listener {
             Light.generateSphere(torchLoc);
         }
     }
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+
+        if (block.getType() == Material.TORCH) {
+            Block torch = event.getBlock();
+            Location torchLoc = torch.getLocation();
+            Sphere Light = new Sphere(5, Material.AIR);
+            Light.generateSphere(torchLoc);
+        }
+    }
 }
+
 
